4dff4fb4a19353853cf7ee40a8122e0c139c4886
Davi Miranda <mirandadavi.mt@gmail.com>
2025-03-21 23:06:30 +0000

    feat: modal and theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  //
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
