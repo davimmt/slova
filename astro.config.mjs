07def038b951893b6e3ca03f20ca4cdb525ed43d
Davi Miranda <mirandadavi.mt@gmail.com>
2025-03-23 18:49:23 +0000

    feat: intro & casos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
+  site: 'https://davimmt.github.io',
+  base: 'slova',
+
   vite: {
     plugins: [tailwindcss()],
   },
